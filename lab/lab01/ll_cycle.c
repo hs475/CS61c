b5810566dcd2bda99fa32c6610f5531b5f6ae25a
hs475 <jcwsjtu417@gmail.com>
2024-07-29 23:09:26 +0800

    finish lab01

--- lab/lab01/ll_cycle.c
+++ lab/lab01/ll_cycle.c
@@ -3,5 +3,13 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    if (head == NULL) return 0;
+    node* tortoise = head, *hare = head;
+    while(hare->next != NULL && hare->next->next != NULL) {
+    	tortoise = tortoise->next;
+		hare = hare->next->next;
+        if (hare == tortoise) return 1;
+	}
     return 0;
 }
+
