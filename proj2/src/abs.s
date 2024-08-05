66111184e0124b13d58c187368d3789aa5a14666
hs475 <jcwsjtu417@gmail.com>
2024-08-06 03:05:53 +0800

    abs

--- proj2/src/abs.s
+++ proj2/src/abs.s
@@ -11,9 +11,15 @@
 abs:
     # Prologue
 
+    # branch if positive
+    bge a0, zero, done
+    
+    # invert a if negative
+    sub a0, zero, a0
+
     # return 0
-    mv a0, zero
+    #mv a0, zero
 
     # Epilogue
-
+done:
     ret
